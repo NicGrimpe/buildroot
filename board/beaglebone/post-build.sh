96021558d29fe94843eb2302177717a7de4f86be
nicG <ngariepy@ideoconcepts.ca>
2023-03-10 12:36:45 -0500

    on a un field buffergit add .

--- board/beaglebone/post-build.sh
+++ board/beaglebone/post-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
+cp $BOARD_DIR/am335x-boneblack.dtb $BINARIES_DIR/am335x-boneblack.dtb
 cp $BOARD_DIR/BB-BONE-4D7C-01-00A1.dtbo $BINARIES_DIR/BB-BONE-4D7C-01-00A1.dtbo
 
 install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
